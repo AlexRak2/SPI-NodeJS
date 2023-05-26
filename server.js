29678a7b503cde764afc711504ed8b3d3e5e81fb
AlexRak2 <56671393+AlexRak2@users.noreply.github.com>
2023-05-26 15:43:04 -0400

    added storage

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.json());
 
+let data = {
+    PumpCount: 3,
+    Pump1Stat: 3,
+    Pump2Stat: 2,
+    Pump3Stat: 1,
+    Level: 9,
+    Flow: 31
+};
+
 app.get('/getData', (req, res) => {
     res.status(200).json(data);
 });
 
 app.post('/updateData', (req, res) => {
-    console.log(req.body + " data"); // Print the updated data to the console
+    data = req.body; // Update the data with the request body
+    console.log(data.Level + " data"); // Print the updated data to the console
 
-    res.status(200).json({ message: 'Data updated successfully. New Level: ' + req.body.Level });
+    res.status(200).json({ message: 'Data updated successfully. New Level: ' + data.Level });
 });
 
 app.listen(port, () => console.log('Server started on port: ' + port));
