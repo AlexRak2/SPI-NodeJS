c805c5962d18651fb7afed972bc7bf0c0ba6873e
AlexRak2 <alejandrorak2@gmail.com>
2023-06-06 14:38:41 -0400

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 443;
 
 const mqtt = require('mqtt');
 const brokerUrl = 'ws://test.mosquitto.org:8080';
